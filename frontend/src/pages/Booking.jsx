461ce127dcbd3f550f4964582e20fbc8ac931517
Bhavesh mishra <bhaveshmishra@Bhaveshs-MacBook-Air-1175.local>
2025-12-11 18:49:13 +0530

    Fix API import case for Vercel build

--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import api from "../Api.js";
+import api from "../api.js";
 
 function Booking() {
   const { id } = useParams();
